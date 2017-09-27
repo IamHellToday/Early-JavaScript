e77bec630156b721c7fe4c22759b046e78005631
Krystian Jabłoński <Krystian Jaboski>
2017-09-28 00:04:49 +0200

    Finishing task 8.1

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var name = prompt('Enter your name'),
+a = 5,
+h = 3,
+triangleArea = a*h/2;
+// Comment
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
+// Triangle
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
